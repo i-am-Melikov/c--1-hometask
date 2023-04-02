45e89271be4b3cd6316ca8caecbf8af0d6f7164d
i-am-Melikov <mahammadmustafaam@code.edu.az>
2023-04-02 16:27:00 +0400

    c#-1 hometask

---
+++ ImtahanPointleriYoxlama/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.Design;
+
+int point = 52;
+if (point <= 100 && point >= 0)
+{
+    if (point <= 100 && point > 90)
+    {
+        Console.WriteLine("A");
+    }
+    else if (point <= 90 && point > 80)
+    {
+        Console.WriteLine("B");
+    }
+    else if (point <= 80 && point > 70)
+    {
+        Console.WriteLine("C");
+    }
+    else if (point <= 70 && point > 60)
+    {
+        Console.WriteLine("D");
+    }
+    else if (point <= 60 && point > 50)
+    {
+        Console.WriteLine("E");
+    }
+    else
+    {
+        Console.WriteLine("Kesildiniz");
+    }
+}
+else
+{
+    Console.WriteLine("Point 100 ve 0 araliginda olmalidir.");
+}
---
+++ MusbetMenfi/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] numbers = { 0, -1, 2, 4, -3, 5, 0, 7, -2, -4, 10 };
+int positive = 0;
+int negative= 0;
+for (int i = 0; i < numbers.Length; i++) 
+{
+    if (numbers[i] == 0)
+    {
+        continue;
+    }
+    else if (numbers[i] > 0)
+                {
+        positive++;
+    }
+    else
+    { negative++; };
+}
+Console.WriteLine(positive);
+Console.WriteLine(negative);
